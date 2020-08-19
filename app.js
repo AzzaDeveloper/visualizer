5ab8f75f6abc2144b7a1763599a55d4debe137af
Azza <azgamedeveloper@gmail.com>
2020-08-19 16:24:06 +0700

    working

--- app.js
+++ app.js
@@ -17,19 +17,46 @@ camera.keysRight.push(68); // S
 // Light
 var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 // Rows of blocks, 25 x 25
-for (i = 0; i < 15; i += 1) {
-    for (j = 0; j < 15; j += 1) {
-        var box = BABYLON.MeshBuilder.CreateBox("box", {size: 1}, scene);
+var blocks = [];
+for (i = 0; i < 20; i += 2) {
+    for (j = 0; j < 20; j += 2) {
+        var box = BABYLON.MeshBuilder.CreateBox("box", {size: 2}, scene);
         box.position.x = j;
         box.position.z = i;
+        blocks.push(box);
     }
 }
 //----------------Audio Handling-----------------//
 
-var buffer = null;
+var audioBuffer = null;
 // Fix up prefixing
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
+var track = null;
+
+var dataArray;
+var analyser = context.createAnalyser;
+
+var ready = false;
+
+function playSound(buffer) {
+    track = context.createBufferSource();
+    track.buffer = audioBuffer;
+    //
+    track.connect(analyser);
+    //
+    const gainNode = context.createGain();
+    gainNode.gain.value = 0.25;
+    track.connect(gainNode).connect(context.destination);
+    // Analyser time
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+    //
+    track.start(0);
+    ready = true;
+}
 function loadSound(url) {
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
@@ -38,28 +65,37 @@ function loadSound(url) {
     // Decode asynchronously
     request.onload = function() {
       context.decodeAudioData(request.response, function(buffer) {
-        dogBarkingBuffer = buffer;
-      }, onError);
+        audioBuffer = buffer;
+        playSound(buffer)
+      }, );
     }
     request.send();
 }
+loadSound("https://raw.githubusercontent.com/AzzaDeveloper/visualizer/master/camellia.mp3");
+console.log(audioBuffer);
 // Creating analyser
 var analyser = context.createAnalyser();
-// Requesting the sound
-const audioElement = document.querySelector('audio');
-audioElement.src = URL.createObjectURL(document.getElementsByTagName('input')[0].files[0]);
 // pass it into the audio context
-const track = context.createMediaElementSource(audioElement);
-track.connect(analyser);
-track.connect(context.destination);
 // Play
-audioElement.play();
 //------------------------------------------------//
+
+function lerp(a, b, t) {
+    return a + (b - a) * t
+}
 
 var t = 0;
 var renderLoop = function() {
     scene.render();
     t -= 0.01;
+    //
+    if (ready) {
+        analyser.getByteTimeDomainData(dataArray);
+        for (i = 0; i < blocks.length; i++) {
+            var height = dataArray[i * 10];
+            var realScale = lerp(blocks[i].scaling.y, height - 128, 0.05);
+            blocks[i].scaling = new BABYLON.Vector3(1, realScale, 1);
+        }
+    }
 };
 engine.runRenderLoop(renderLoop);
 
